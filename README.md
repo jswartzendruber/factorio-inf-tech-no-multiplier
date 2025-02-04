456a927c0be525c1db5f1c3e1ddce5ccd90af902
jswartzendruber <jsw695538@gmail.com>
2025-02-04 18:11:37 -0500

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# No Science Cost Multipler on Infinite Technologies
+
+This is a tiny factorio mod that turns off the science multiplier for infinite technologies.
+
+If you have a 10x science cost multipler set, mining productivity 1 will cost 25k science, 2 will cost 50k, and 3 will start at 1k and then scale normally from there.
---
+++ data-final-fixes.lua
@@ -0,0 +1,26 @@
+-- infinite research generally has a finite number of defined 'levels', ones that don't have a formula. this function does not account for them.
+function is_infinite_research(name)
+    return data.raw.technology[name].max_level == "infinite";
+end
+
+for name, technology in pairs(data.raw.technology) do
+    -- skip trigger technology
+    if (technology.research_trigger ~= nil) then
+        goto continue;
+    end
+
+    -- debug printing
+    -- if is_infinite_research(name) then
+    --     log(name .. " INFINITE")
+    -- else
+    --     log(name .. " default")
+    -- end
+
+    -- if infinite, set the cost to what it was before the research multiplier was applied
+    if is_infinite_research(name) then
+        technology.ignore_tech_cost_multiplier = true
+        -- log(name .. " : ignore_tech_cost_multiplier = true")
+    end
+
+    ::continue::
+end
